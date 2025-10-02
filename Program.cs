bc936b3de33fa6ed9bc231bfb59c52d571f98dd3
Christof Goletzko <c.goletzko@iropa.de>
2025-10-02 12:38:55 +0200

    Initial Commit MCP

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MCP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Master Control Programm gestartet...");
+
+            // 1. Prüfen, ob überhaupt Argumente übergeben wurden
+        if (args.Length == 0)
+        {
+        }
+
+        // Der erste Eintrag ist der Befehl (z.B. "connect")
+        string command = args[0].ToLowerInvariant();
+
+        // 2. Das Switch-Statement (Die Dispatch-Tabelle)
+        switch (command)
+        {
+            case "connect":
+                Console.WriteLine("Master Control Programm gestartet...");   
+                break;
+            default:
+                Console.WriteLine($"Fehler: Unbekannter Befehl '{command}'.");
+                return;
+        }
+        }
+    }
+}
