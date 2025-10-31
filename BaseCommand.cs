143b0c1def504f644a7f9163db1c881d197ca80a
Christof Goletzko <c.goletzko@iropa.de>
2025-10-31 13:06:36 +0100

    BaseCommand ausgelagert

---
+++ BaseCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP
+{
+	public abstract class BaseCommand
+	{
+		protected abstract string Parameters { get; }
+
+		public virtual void ShowHelp()
+		{
+			Console.WriteLine("Mögliche Parameter = {0}", Parameters);
+		}
+	}
+}
